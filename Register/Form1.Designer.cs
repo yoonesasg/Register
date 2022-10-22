225d1e4529b76ee4f1ca2cefaf73d278802da649
Yoones Asgarian <yoonesasgasg@gmail.com>
2022-10-22 09:38:51 +0330

    add files

---
+++ Register/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Register
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFullName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblFullNameError = new System.Windows.Forms.Label();
+            this.lblUserNameError = new System.Windows.Forms.Label();
+            this.lblEmailError = new System.Windows.Forms.Label();
+            this.lblPasswordError = new System.Windows.Forms.Label();
+            this.lblConfirmError = new System.Windows.Forms.Label();
+            this.lblSuccess = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtFullName
+            // 
+            this.txtFullName.Location = new System.Drawing.Point(12, 12);
+            this.txtFullName.Name = "txtFullName";
+            this.txtFullName.Size = new System.Drawing.Size(237, 31);
+            this.txtFullName.TabIndex = 0;
+            this.txtFullName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFullName_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(255, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = ":نام و نام خانوادگی";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(255, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = ": نام کاربری";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(12, 81);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(237, 31);
+            this.txtUsername.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(255, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = ": ایمیل";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(12, 156);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(237, 31);
+            this.txtEmail.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(255, 237);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = ": پسورد";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(12, 234);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(237, 31);
+            this.txtPassword.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(255, 311);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = ": تکرار پسورد";
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.Location = new System.Drawing.Point(12, 308);
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.Size = new System.Drawing.Size(237, 31);
+            this.txtConfirmPassword.TabIndex = 8;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(295, 367);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(112, 34);
+            this.btnSubmit.TabIndex = 10;
+            this.btnSubmit.Text = "ثبت";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblFullNameError
+            // 
+            this.lblFullNameError.AutoSize = true;
+            this.lblFullNameError.ForeColor = System.Drawing.Color.Red;
+            this.lblFullNameError.Location = new System.Drawing.Point(12, 50);
+            this.lblFullNameError.Name = "lblFullNameError";
+            this.lblFullNameError.Size = new System.Drawing.Size(0, 25);
+            this.lblFullNameError.TabIndex = 11;
+            // 
+            // lblUserNameError
+            // 
+            this.lblUserNameError.AutoSize = true;
+            this.lblUserNameError.ForeColor = System.Drawing.Color.Red;
+            this.lblUserNameError.Location = new System.Drawing.Point(12, 122);
+            this.lblUserNameError.Name = "lblUserNameError";
+            this.lblUserNameError.Size = new System.Drawing.Size(0, 25);
+            this.lblUserNameError.TabIndex = 12;
+            // 
+            // lblEmailError
+            // 
+            this.lblEmailError.AutoSize = true;
+            this.lblEmailError.ForeColor = System.Drawing.Color.Red;
+            this.lblEmailError.Location = new System.Drawing.Point(12, 199);
+            this.lblEmailError.Name = "lblEmailError";
+            this.lblEmailError.Size = new System.Drawing.Size(0, 25);
+            this.lblEmailError.TabIndex = 13;
+            // 
+            // lblPasswordError
+            // 
+            this.lblPasswordError.AutoSize = true;
+            this.lblPasswordError.ForeColor = System.Drawing.Color.Red;
+            this.lblPasswordError.Location = new System.Drawing.Point(12, 280);
+            this.lblPasswordError.Name = "lblPasswordError";
+            this.lblPasswordError.Size = new System.Drawing.Size(0, 25);
+            this.lblPasswordError.TabIndex = 14;
+            // 
+            // lblConfirmError
+            // 
+            this.lblConfirmError.AutoSize = true;
+            this.lblConfirmError.ForeColor = System.Drawing.Color.Red;
+            this.lblConfirmError.Location = new System.Drawing.Point(18, 350);
+            this.lblConfirmError.Name = "lblConfirmError";
+            this.lblConfirmError.Size = new System.Drawing.Size(0, 25);
+            this.lblConfirmError.TabIndex = 15;
+            // 
+            // lblSuccess
+            // 
+            this.lblSuccess.AutoSize = true;
+            this.lblSuccess.ForeColor = System.Drawing.Color.Lime;
+            this.lblSuccess.Location = new System.Drawing.Point(12, 367);
+            this.lblSuccess.Name = "lblSuccess";
+            this.lblSuccess.Size = new System.Drawing.Size(0, 25);
+            this.lblSuccess.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 413);
+            this.Controls.Add(this.lblSuccess);
+            this.Controls.Add(this.lblConfirmError);
+            this.Controls.Add(this.lblPasswordError);
+            this.Controls.Add(this.lblEmailError);
+            this.Controls.Add(this.lblUserNameError);
+            this.Controls.Add(this.lblFullNameError);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtConfirmPassword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFullName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtFullName;
+        private Label label1;
+        private Label label2;
+        private TextBox txtUsername;
+        private Label label3;
+        private TextBox txtEmail;
+        private Label label4;
+        private TextBox txtPassword;
+        private Label label5;
+        private TextBox txtConfirmPassword;
+        private Button btnSubmit;
+        private Label lblFullNameError;
+        private Label lblUserNameError;
+        private Label lblEmailError;
+        private Label lblPasswordError;
+        private Label lblConfirmError;
+        private Label lblSuccess;
+    }
+}
---
+++ Register/Form1.cs
@@ -0,0 +1,141 @@
+﻿namespace Register
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            Resicle();
+            var result = Validate();
+            if (result)
+            {
+                result = ValidateUserName();
+                if (result)
+                {
+                    result = PasswordSecure();
+                    if (result)
+                    {
+                        result = ValidateEmail();
+                        if (result)
+                        {
+                            lblSuccess.Text = "اطلاعات با موفقیت ثبت شد";
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private bool Validate()
+        {
+            bool flag = true;
+            if (txtFullName.Text == "")
+            {
+                lblFullNameError.Text = "لطفا نام و نام خانوادگی را وارد کنید ";
+                flag = false;
+            }
+            if (txtUsername.Text == "")
+            {
+                lblUserNameError.Text = "لطفا نام کاربری را وارد کنید ";
+                flag = false;
+            }
+            if (txtEmail.Text == "")
+            {
+                lblEmailError.Text = "لطفا ایمیل را وارد کنید ";
+                flag = false;
+            }
+            if (txtPassword.Text == "")
+            {
+                lblPasswordError.Text = "لطفا پسورد را وارد کنید ";
+                flag = false;
+            }
+            if (txtConfirmPassword.Text == "")
+            {
+                lblConfirmError.Text = "لطفا تکرار کلمه عبور را وارد کنید ";
+                flag = false;
+            }
+
+            return flag;
+        }
+
+        private void Resicle()
+        {
+            lblConfirmError.Text = "";
+            lblPasswordError.Text = "";
+            lblEmailError.Text = "";
+            lblFullNameError.Text = "";
+            lblUserNameError.Text = "";
+        }
+
+        private void txtFullName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private bool PasswordSecure()
+        {
+            if (txtPassword.Text.Length < 8 || txtPassword.Text.Length > 16)
+            {
+                lblPasswordError.Text = "پسورد باید بین 8 تا 16 کارکتر باشد";
+                return false;
+            }
+
+            if (txtPassword.Text != txtConfirmPassword.Text)
+            {
+                lblConfirmError.Text = "کلمه های عبور با هم مغایرت دارند";
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool ValidateEmail()
+        {
+            string email = txtEmail.Text;
+
+            System.Text.RegularExpressions.Regex expr = new System.Text.RegularExpressions.Regex(@"^[a-zA-Z][\w\.-]{2,28}[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$");
+
+            if (expr.IsMatch(email))
+                return true;
+
+            else
+            {
+                lblEmailError.Text = "ایمیل معتبر نمیباشد";
+                return false;
+            }
+        }
+
+        private bool ValidateUserName()
+        {
+            if (!txtUsername.Text.Any(char.IsDigit))
+            {
+                lblUserNameError.Text = "نام کاربری باید شامل عدد و حروف باشد";
+                return false;
+            }
+            if (!txtUsername.Text.Any(char.IsLetter))
+            {
+                lblUserNameError.Text = "نام کاربری باید شامل عدد و حروف باشد";
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
